918d3c203a427efb57e34b4cc78cd1e13885f6de
johnpaulvaughan <johnpaulvaughan@hotmail.com>
2016-11-15 15:27:00 +1100

    converting to es6 with es5 transpile

--- index.ts
+++ index.ts
@@ -1,21 +1,19 @@
-"use strict";
 /**
  * return a hello world string.
  *
  * @param  null
  * @return {String}
  */
-function helloWorld() {
-    return String('hello world');
+export function helloWorld() {
+  return String('hello world');
 }
-exports.helloWorld = helloWorld;
+
 /**
  * return a hello world string with a custom name instead of 'world'.
  *
  * @param  {String} name
  * @return {String}
  */
-function helloYou(name) {
-    return String('hello ' + name);
+export function helloYou(name) {
+  return String('hello ' + name);
 }
-exports.helloYou = helloYou;
