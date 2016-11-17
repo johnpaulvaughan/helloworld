d12c79e6407afad0dea8f89b5c127afb61a018bd
johnpaulvaughan <johnpaulvaughan@hotmail.com>
2016-11-17 12:33:23 +1100

    converted output to es6 format

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-"use strict";
 /**
  * return a hello world string.
  *
  * @param  null
  * @return {String}
  */
-function helloWorld() {
+export function helloWorld() {
     return String('hello world');
 }
-exports.helloWorld = helloWorld;
 /**
  * return a hello world string with a custom name instead of 'world'.
  *
  * @param  {String} name
  * @return {String}
  */
-function helloYou(name) {
+export function helloYou(name) {
     return String('hello ' + name);
 }
-exports.helloYou = helloYou;
+//# sourceMappingURL=index.js.map
